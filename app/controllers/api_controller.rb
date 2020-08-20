1cfab853d4dd39822fdcc2e2ce1cf8d622166910
xemnikk <xemnikk@gmail.com>
2020-08-20 10:14:38 -0500

    move

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,23 @@
+class ApiController < ApplicationController
+
+    require 'uri'
+    require 'net/http'
+    require 'openssl'
+
+
+    def request_api
+        url = URI("https://amazon-products1.p.rapidapi.com/offers?type=LIGHTNING_DEAL&max_number=100&min_number=5&country=US")
+
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+        request = Net::HTTP::Get.new(url)
+        request["x-rapidapi-host"] = 'amazon-products1.p.rapidapi.com'
+        request["x-rapidapi-key"] = '77bc85f9femsh8cc7ee10e72cfafp1ccc58jsna4131bc1c32a'
+
+        response = http.request(request)
+        render json: response.body
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :reviews
   resources :likes
   resources :products
+  get '/api' => 'api#request_api'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
